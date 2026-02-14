788f9637c31e551555fdb5fe43bb2cf6f9f7995e
Parth Tripathi <tparth2409@gmail.com>
2026-02-15 04:28:55 +0530

    theme

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,11 @@
 import { LoginForm } from "@/features/auth/components/login-form";
 import { requireUnauth } from "@/lib/auth-utils";
+import Link from "next/link";
+import Image from "next/image";
 
 const Page = async() => {
     await requireUnauth();
-    return (
-    <div>
-        <LoginForm/>
-    </div>
+    return (<LoginForm />
     );
 };
 export default Page;
